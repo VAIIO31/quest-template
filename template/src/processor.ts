f84b1948641b0e5e89800ed9d844ffaceb15b7fd
VAIIO31 <113572522+VAIIO31@users.noreply.github.com>
2023-11-05 12:16:07 +0700

    Update processor.ts

--- template/src/processor.ts
+++ template/src/processor.ts
@@ -1,5 +1,5 @@
-import {EvmBatchProcessor} from '@subsquid/evm-processor'
-import {lookupArchive} from '@subsquid/archive-registry'
+import { EvmBatchProcessor } from '@subsquid/evm-processor';
+import { lookupArchive } from '@subsquid/archive-registry';
 
 export const processor = new EvmBatchProcessor()
     .setDataSource({
@@ -8,8 +8,11 @@ export const processor = new EvmBatchProcessor()
     })
     .setFinalityConfirmation(10)
     .setFields({
-        // specify field selection here
+        id: true,
+        owner: true,
+        displayName: true,
+        imageUrl: true
     })
     .addLog({
         address: ['0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB']
-    })
+    });
